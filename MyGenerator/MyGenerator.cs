21011362c46a690011f4ce8ccb02305dc0262281
Miroslav Jezik <mjezik@microsoft.com>
2025-03-04 17:09:16 +0100

    Add repro

---
+++ MyGenerator/MyGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MyGenerator;
+
+[Generator]
+public class MyGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+        INamedTypeSymbol? classSymbol = null;
+        classSymbol?.GetAttributes();
+    }
+}
